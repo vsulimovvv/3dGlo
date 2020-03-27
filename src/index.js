16d1d14d1761e7408bdffa6b7f1f103b845db1ac
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-03-27 20:54:16 +0300

    lesson27

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 'use strict';
 
-import timer from './modules/timer'
-import toggleMenu from './modules/toggleMenu'
-import popup from './modules/popup'
-import tabs from './modules/tabs'
-import slider from './modules/slider'
-import changeImg from './modules/changeImg'
-import calc from './modules/calc'
-import sendForm from './modules/sendForm'
-import validAllForm from './modules/validAllForm'
+import  '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+
+import timer from './modules/timer';
+import toggleMenu from './modules/toggleMenu';
+import popup from './modules/popup';
+import tabs from './modules/tabs';
+import slider from './modules/slider';
+import changeImg from './modules/changeImg';
+import calc from './modules/calc';
+import sendForm from './modules/sendForm';
+import validAllForm from './modules/validAllForm';
 
 // Timer
 timer();
