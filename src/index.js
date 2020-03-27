260f67ad1fbbef2701fee6edf713a5273a5890a4
vsulimovvv <60267032+vsulimovvv@users.noreply.github.com>
2020-03-27 18:42:28 +0300

    lesson27

---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import timer from './modules/timer'
+import toggleMenu from './modules/toggleMenu'
+import popup from './modules/popup'
+import tabs from './modules/tabs'
+import slider from './modules/slider'
+import changeImg from './modules/changeImg'
+import calc from './modules/calc'
+import sendForm from './modules/sendForm'
+import validAllForm from './modules/validAllForm'
+
+// Timer
+timer();
+// модальное окно
+toggleMenu();
+// Табы
+tabs();
+// Popup
+popup();
+// slider
+slider();
+// смена картинок
+changeImg();
+// calculate 
+calc();
+// валидация
+validAllForm();
+// send AJAX form
+sendForm();
---
+++ src/modules/changeImg.js
@@ -0,0 +1,15 @@
+const changeImg = () => {
+  const command = document.querySelector('.command');
+  let src;
+  command.addEventListener('mouseover', event => {
+    if (event.target.matches('img')) {
+      src = event.target.getAttribute('src');
+      event.target.src = event.target.dataset.img;
+    }
+  });
+  command.addEventListener('mouseout', event => {
+    event.target.src = src;
+  });
+}
+
+export default changeImg;
